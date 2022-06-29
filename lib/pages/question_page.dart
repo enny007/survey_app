7b27fe25b04f565dbcafee7ea396cfce4c3cf655
Eniola Ojo <eniolaojo701@gmail.com>
2022-06-29 05:32:14 -0700

    changes to main

---
+++ lib/pages/question_page.dart
@@ -0,0 +1,139 @@
+import 'package:flutter/material.dart';
+import 'package:survey_app/models/cervical_cancer_survey.dart';
+
+class QuestionPage extends StatefulWidget {
+  const QuestionPage({Key? key}) : super(key: key);
+
+  @override
+  State<QuestionPage> createState() => _QuestionPageState();
+}
+
+class _QuestionPageState extends State<QuestionPage> {
+  // ignore: prefer_final_fields
+  PageController? _controller = PageController(initialPage: 0);
+  Color bottonColor = Colors.white;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0xff0444C0).withOpacity(0.7),
+      body: Padding(
+        padding: const EdgeInsets.all(18),
+        child: PageView.builder(
+            physics: const NeverScrollableScrollPhysics(),
+            controller: _controller!,
+            itemCount: questions.length,
+            itemBuilder: (context, index) {
+              return Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  SizedBox(
+                    width: double.infinity,
+                    child: Text(
+                      'Question ${index + 1} /${questions.length}',
+                      style: const TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.w300,
+                          fontSize: 20),
+                    ),
+                  ),
+                  const Divider(
+                    color: Colors.white,
+                    height: 8.0,
+                    thickness: 2,
+                  ),
+                  const SizedBox(
+                    height: 15,
+                  ),
+                  Text(
+                    questions[index].question!,
+                    style: const TextStyle(
+                      color: Colors.white,
+                      fontSize: 28,
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 25,
+                  ),
+                  for (int i = 0; i < questions[index].answers!.length; i++)
+                    Container(
+                      width: double.infinity,
+                      margin: const EdgeInsets.only(bottom: 18),
+                      child: MaterialButton(
+                        shape: const StadiumBorder(),
+                        color: questions[index].selectedAnswer == i
+                            ? Colors.yellow
+                            : bottonColor,
+                        padding: const EdgeInsets.symmetric(vertical: 18),
+                        onPressed: () {
+                          //provider
+                          setState(() {
+                            questions[index].selectedAnswer = i;
+                          });
+                        },
+                        child: Text(
+                          questions[index].answers![i],
+                          style: const TextStyle(
+                            fontSize: 20,
+                            fontWeight: FontWeight.w500,
+                          ),
+                        ),
+                      ),
+                    ),
+                  const SizedBox(
+                    height: 30,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      OutlinedButton(
+                        style: OutlinedButton.styleFrom(
+                          shape: const StadiumBorder(),
+                          side: const BorderSide(
+                            color: Colors.white,
+                            width: 2,
+                          ),
+                        ),
+                        onPressed: () {
+                          _controller!.previousPage(
+                            duration: const Duration(milliseconds: 1000),
+                            curve: Curves.easeInOut,
+                          );
+                        },
+                        child: const Text(
+                          'Previous Question',
+                          style: TextStyle(color: Colors.white),
+                        ),
+                      ),
+                      OutlinedButton(
+                        style: OutlinedButton.styleFrom(
+                          shape: const StadiumBorder(),
+                          side: const BorderSide(
+                            color: Colors.white,
+                            width: 2,
+                          ),
+                        ),
+                        onPressed: index + 1 == questions.length
+                            ? () {}
+                            : () {
+                                _controller!.nextPage(
+                                  duration: const Duration(milliseconds: 1000),
+                                  curve: Curves.easeInOut,
+                                );
+                              },
+                        child: Text(
+                          index + 1 == questions.length
+                              ? 'Submit'
+                              : 'Next Question',
+                          style: const TextStyle(color: Colors.white),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              );
+            }),
+      ),
+    );
+  }
+}
